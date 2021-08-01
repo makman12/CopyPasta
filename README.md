59bf452142c6ee2fef8e72818fe94a3a2dd05c1d
makman12h <makman12h@gmail.com>
2021-08-01 13:04:39 +0300

    added icon and imp.

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# copypasta
+# CopyPasta
 
-CopyPasta is a VsCode Extension allows you to copy paste first answer to your question from Stack OverFlow
+CopyPasta is a VsCode Extension allows you to copy paste the code section first answer to your question from Stack OverFlow.
 
 ## Features
 
-In your text editor just write what you would normally search on google, selected it then press ctrl+enter you will get what you want. 
+In your text editor just write what you would normally search on Google, then press 'ctrl+enter' you will get what you want. You don't have to add name of the language you are writing on because it will be automatically added.
+
+Currently there two advanced options;
+
+- [x] If you add '-s' to the end of the query sentence it will open the page for you.  
+    - [x]Also if you just write '-s' and press ctrl+enter it will open the page for your last search.
+
+- [x]Command will bring you the code section of the first comment if you want to get other comments you can just call it by number and then press ctrl+enter so if you just searched for a query and then wanted to see second comments code section you have to just type '2' and then press ctrl+enter. No need to searched it again. 
+
+
+## Usage
+Obviously if you have spesific enough problems you should check them out by yourself but if you just wanna know the syntax that you used for CopyPasta is your helper.
--- extension.js
+++ extension.js
@@ -10,44 +10,82 @@ const clipboardy = require('clipboardy');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+
+function isInt(value) {
+	return !isNaN(value) &&
+		parseInt(Number(value)) == value &&
+		!isNaN(parseInt(value, 10));
+}
+
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copypasta" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	let query;
+	let link;
+	let links;
 	let disposable = vscode.commands.registerCommand('copypasta.copypasta', function () {
 		let language = vscode.window.activeTextEditor.document.languageId
-		console.log(language)
-
+		let count = 1;
+		let activeLine = vscode.window.activeTextEditor.selection.active.line;
+		vscode.window.activeTextEditor.selection.active.line = activeLine + 1
+		const mali = vscode.window.activeTextEditor.document.lineAt(activeLine)
+		console.log(mali._text)
 		vscode.commands.executeCommand("editor.action.clipboardCutAction").then(() => {
 
 			vscode.env.clipboard.readText().then((text) => {
-				let clip = text;
-				let query = clip;
-				/* code */
-				console.log(query)
+				console.log("bu text", text)
+				let clip = text.trim();
+				console.log(clip, clip == "-s")
+				if (clip == "-s") {
+					console.log("göster istoyr")
+					vscode.env.openExternal(vscode.Uri.parse(link));
 
-				query = language + "+stackoverflow+" + query.replace(" ", "+")
-				console.log(query)
-				let url1 = "https://www.google.com/search?q=" + query
-				getGoogle(url1)
+				} else if (isInt(clip)) {
+					console.log("buraya geldi")
+					let url1 = "https://www.google.com/search?q=" + query
+					getGoogle(url1, false, clip)
+				} else {
+
+
+					query = clip;
+					let open;
+					if (query.substr(-2) == "-s") {
+						open = true
+						query = query.split("-s")[0]
+					} else {
+						open = false
+					}
+					if (query.substr(-3).includes("-g")) {
+
+						count = query.substr(-1)
+						console.log(count)
+						query = query.split("-g")[0]
+					}
+					query = language + "+stackoverflow+" + query.replaceAll(" ", "+")
+					let url1 = "https://www.google.com/search?q=" + query
+					getGoogle(url1, open, count)
+				}
 			});
 		})
 
 
 
 
-		function getGoogle(url1) {
+		function getGoogle(url1, open, count) {
 			rp(url1).then(html => {
 				let $ = cheerio.load(html)
-				let link = $("h3").first().parent().attr("href")
-				link = link.split("=")[1].split("&sa")[0]
-				scrapeCode(link)
-				vscode.window.showInformationMessage('CopyPasta!');
+				links = []
+				$("h3").each((i, e) => {
+					let l = e.parent.attribs.href
+					l = l.split("=")[1].split("&sa")[0]
+					if (l.includes("stackoverflow.com")) {
+						links.push(l)
+					}
+
+				})
+				link = links[0]
+				scrapeCode(link, open, count)
+
 
 			})
 		}
@@ -58,20 +96,41 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 
-function scrapeCode(link) {
+function scrapeCode(link, open, count = 1) {
+
 	rp(link)
 		.then(function (html) {
 			//success!
 			let $ = cheerio.load(html)
 			let list = []
-			$(".accepted-answer").find(".answercell").find("code").each(function (index, element) {
-				list.push($(element).text())
+			$(".answercell").find("code").each(function (index, element) {
+				list.push($(element).text().replace(">>> ", ""))
 			})
-			let code = $(".answercell").first().find("code").text()
-			code = code.replace(">>> ", "")
-			clipboardy.writeSync(code)
-			console.log(code)
-			vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+
+			if (count == "A") {
+				list = list.join("\n")
+				list = "\n" + list
+				clipboardy.writeSync(list)
+				vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+				vscode.window.showInformationMessage("CopyPasta is Ready!");
+			} else {
+				let code = list[count - 1]
+				if (code) {
+					code = "\n" + list[count - 1]
+					clipboardy.writeSync(code)
+					vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+					vscode.window.showInformationMessage("CopyPasta is Ready!");
+
+				} else {
+					vscode.window.showInformationMessage("CopyPasta is not healthy for you!, I just couldn't find it :(");
+				}
+
+			}
+
+
+			if (open) {
+				vscode.env.openExternal(vscode.Uri.parse(link));
+			}
 		})
 		.catch(function () {
 			//handle error
