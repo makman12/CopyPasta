782806ecfd685b57cee10cbfdeccfa93d36ddc25
makman12h <makman12h@gmail.com>
2021-08-03 15:19:15 +0300

    return list

--- extension.js
+++ extension.js
@@ -18,18 +18,19 @@ function isInt(value) {
 		!isNaN(parseInt(value, 10));
 }
 
+let link;
+let links;
+let list;
+
 function activate(context) {
 
 	let query;
-	let link;
-	let links;
+
 	let disposable = vscode.commands.registerCommand('copypasta.copypasta', function () {
 		let language = vscode.window.activeTextEditor.document.languageId
 		let count = 1;
 		let activeLine = vscode.window.activeTextEditor.selection.active.line;
 		vscode.window.activeTextEditor.selection.active.line = activeLine + 1
-		const mali = vscode.window.activeTextEditor.document.lineAt(activeLine)
-		console.log(mali._text)
 		vscode.commands.executeCommand("editor.action.clipboardCutAction").then(() => {
 
 			vscode.env.clipboard.readText().then((text) => {
@@ -37,11 +38,12 @@ function activate(context) {
 				let clip = text.trim();
 				console.log(clip, clip == "-s")
 				if (clip == "-s") {
-					console.log("göster istoyr")
 					vscode.env.openExternal(vscode.Uri.parse(link));
 
+				} else if (clip == "-l") {
+					getQuickList()
 				} else if (isInt(clip)) {
-					console.log("buraya geldi")
+
 					let url1 = "https://www.google.com/search?q=" + query
 					getGoogle(url1, false, clip)
 				} else {
@@ -68,73 +70,81 @@ function activate(context) {
 			});
 		})
 
-
-
-
-		function getGoogle(url1, open, count) {
-			rp(url1).then(html => {
-				let $ = cheerio.load(html)
-				links = []
-				$("h3").each((i, e) => {
-					let l = e.parent.attribs.href
-					l = l.split("=")[1].split("&sa")[0]
-					if (l.includes("stackoverflow.com")) {
-						links.push(l)
-					}
-
-				})
-				link = links[0]
-				scrapeCode(link, open, count)
-
-
-			})
-		}
-
-
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+function getGoogle(url1, open, count) {
+	rp(url1).then(html => {
+		let $ = cheerio.load(html)
+		links = []
+		$("h3").each((i, e) => {
+			let l = e.parent.attribs.href
+			l = l.split("=")[1].split("&sa")[0]
+			if (l.includes("stackoverflow.com")) {
+				links.push(l)
+			}
+
+		})
+		link = links[0]
+		scrapeCode(link, open, count)
+
+
+	})
+}
+
 function scrapeCode(link, open, count = 1) {
+	if (count != 1) {
+		let code = list[count - 1]
+		if (code) {
+			code = "\n" + list[count - 1]
+			clipboardy.writeSync(code)
+			vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+			vscode.window.showInformationMessage("CopyPasta is Ready!");
 
-	rp(link)
-		.then(function (html) {
-			//success!
-			let $ = cheerio.load(html)
-			let list = []
-			$(".answercell").find("code").each(function (index, element) {
-				list.push($(element).text().replace(">>> ", ""))
-			})
+		} else {
+			vscode.window.showInformationMessage("CopyPasta is not healthy for you!, I just couldn't find it :(");
+		}
 
-			if (count == "A") {
-				list = list.join("\n")
-				list = "\n" + list
-				clipboardy.writeSync(list)
-				vscode.commands.executeCommand("editor.action.clipboardPasteAction")
-				vscode.window.showInformationMessage("CopyPasta is Ready!");
-			} else {
-				let code = list[count - 1]
-				if (code) {
-					code = "\n" + list[count - 1]
-					clipboardy.writeSync(code)
+	} else {
+
+
+		rp(link)
+			.then(function (html) {
+				//success!
+				let $ = cheerio.load(html)
+				list = []
+				$(".answercell").find("pre > code").each(function (index, element) {
+					list.push($(element).text().replace(">>> ", ""))
+				})
+				if (list.length > 1) {
+					vscode.window.showQuickPick(list).then(seçim => {
+						console.log(seçim)
+						clipboardy.writeSync(seçim)
+						vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+					})
+
+				} else if (list.length == 1) {
+					clipboardy.writeSync(list[0])
 					vscode.commands.executeCommand("editor.action.clipboardPasteAction")
-					vscode.window.showInformationMessage("CopyPasta is Ready!");
-
 				} else {
 					vscode.window.showInformationMessage("CopyPasta is not healthy for you!, I just couldn't find it :(");
 				}
+			})
+			.catch(function () {
+				//handle error
+			});
+	}
+}
 
-			}
 
-
-			if (open) {
-				vscode.env.openExternal(vscode.Uri.parse(link));
-			}
-		})
-		.catch(function () {
-			//handle error
-		});
+function getQuickList() {
+	vscode.window.showQuickPick(list).then(seçim => {
+		console.log(seçim)
+		clipboardy.writeSync(seçim)
+		vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+	})
 }
 
 
